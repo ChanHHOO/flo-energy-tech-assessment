954a89cd84e854df27e6a5ea38625854912104b9
ChanHHOO <hpyho33@kookmin.ac.kr>
2025-10-26 23:57:25 +0900

    fix : Remove 500record validate code

--- src/test/kotlin/com/flo/nem12/service/impl/NEM12ParserServiceImplTest.kt
+++ src/test/kotlin/com/flo/nem12/service/impl/NEM12ParserServiceImplTest.kt
@@ -82,32 +82,6 @@ class NEM12ParserServiceImplTest {
     }
 
     @Test
-    fun `should throw exception when NMI block is not closed`() {
-        // Given
-        val inputFile = tempDir.resolve("unclosed_nmi.nem12")
-        val invalidContent =
-            """
-            100,NEM12,202401011200,SENDER,RECEIVER
-            200,1234567890,E1,1,E1,N1,01009,KWH,30,20240101
-            300,20240101,10.5,11.2,12.3,13.4,14.5,15.6,16.7,17.8,18.9,19.0,20.1,21.2,22.3,23.4,24.5,25.6,26.7,27.8,28.9,29.0,30.1,31.2,32.3,33.4,34.5,35.6,36.7,37.8,38.9,39.0,40.1,41.2,42.3,43.4,44.5,45.6,46.7,47.8,48.9,49.0,50.1,51.2,52.3,53.4,54.5,55.6,56.7,57.8,A,,,20050301120000,
-            900
-            """.trimIndent()
-        Files.writeString(inputFile, invalidContent)
-
-        val repository = TestMeterReadingRepository()
-        val failureRepository = TestFailureReadingsRepository()
-        val failureHandler = DatabaseFailureHandler(failureRepository)
-        val recordParserService = RecordParserServiceImpl(failureHandler)
-        val service = NEM12ParserServiceImpl(repository, recordParserService)
-        val cmd = NEM12ParseCommand(inputFile, tempDir.resolve("output.db"))
-
-        // When & Then
-        assertThrows<ParseException> {
-            service.parseFile(cmd)
-        }
-    }
-
-    @Test
     fun `should throw exception when 300 record appears outside NMI block`() {
         // Given
         val inputFile = tempDir.resolve("300_outside_block.nem12")
